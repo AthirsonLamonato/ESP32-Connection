fe4e4e04bb8473c34b4327edf1684f97cf2c943f
eduardohuther <39381045+eduardohuther@users.noreply.github.com>
2023-09-25 17:38:01 -0300

    Update AccessPoint.ino

--- AccessPoint.ino
+++ AccessPoint.ino
@@ -1,13 +1,30 @@
-// https://www.upesy.com/blogs/tutorials/how-create-a-wifi-acces-point-with-esp32#
 #include <WiFi.h>
 #include "esp_wifi.h"
 
-const char* ssid           = "uPesy_AP";                // SSID Name
-const char* password       = "super_strong_password";   // SSID Password - Set to NULL to have an open AP
-const int   channel        = 10;                        // WiFi Channel number between 1 and 13
-const bool  hide_SSID      = false;                     // To disable SSID broadcast -> SSID will not appear in a basic WiFi scan
-const int   max_connection = 2;                         // Maximum simultaneous connected clients on the AP
+const char* ssid           = "ESP_Teste";                // Nome do SSID
+const char* password       = "teste12345";               // Senha do SSID -> null = sem senha
+const int   channel        = 1;                         // WiFi Channel -> entre 1 e 13
+const bool  hide_SSID      = false;                     // Se true, SSID não irá aparecer em scan básico de WiFi
+const int   max_connection = ESP_WIFI_MAX_CONN_NUM;     // Número máximo de clientes conectados (15)
 
+void setup()
+{
+    Serial.begin(115200); // velocidade da serial
+    Serial.println("----- CRIANDO AP");
+    WiFi.onEvent(WiFiEvent); //método que monitora os eventos do WiFi
+    WiFi.mode(WIFI_AP); //seta o modo do WiFi para AP
+    WiFi.softAP(ssid, password, channel, hide_SSID, max_connection); // configura o AP
+    WiFi.enableAP(true); //ativa o AP
+    Serial.print("----- AP criado com o IP de gateway ");
+    Serial.println(WiFi.softAPIP());
+}
+
+void loop(){
+  display_connected_devices();
+  delay(1000);
+}
+
+// método para mostrar os dispositivos conectados (MAC e IP)
 void display_connected_devices()
 {
     wifi_sta_list_t wifi_sta_list;
@@ -16,28 +33,32 @@ void display_connected_devices()
     tcpip_adapter_get_sta_list(&wifi_sta_list, &adapter_sta_list);
 
     if (adapter_sta_list.num > 0)
-        Serial.println("-----------");
-    for (uint8_t i = 0; i < adapter_sta_list.num; i++)
-    {
+        Serial.println("----------- -----------");
+    for (uint8_t i = 0; i < adapter_sta_list.num; i++) {
         tcpip_adapter_sta_info_t station = adapter_sta_list.sta[i];
-        Serial.print((String)"[+] Device " + i + " | MAC : ");
+        Serial.print((String)"Dispositivo " + i + " | MAC : ");
         Serial.printf("%02X:%02X:%02X:%02X:%02X:%02X", station.mac[0], station.mac[1], station.mac[2], station.mac[3], station.mac[4], station.mac[5]);
-        Serial.println((String) " | IP " + ip4addr_ntoa(&(station.ip)));
+        char str_ip[16];
+        esp_ip4addr_ntoa(&(station.ip), str_ip, IP4ADDR_STRLEN_MAX);
+        Serial.println((String) " | IP " + str_ip);
     }
 }
 
-void setup()
-{
-    Serial.begin(115200);
-    Serial.println("\n[*] Creating AP");
-    WiFi.mode(WIFI_AP);
-    WiFi.softAP(ssid, password, channel, hide_SSID, max_connection);
-    Serial.print("[+] AP Created with IP Gateway ");
-    Serial.println(WiFi.softAPIP());
-}
-
-void loop()
-{
-    display_connected_devices();
-    delay(5000);
+// método que monitora os eventos do WiFi
+void WiFiEvent(WiFiEvent_t event, WiFiEventInfo_t info){
+    switch (event) {
+        case ARDUINO_EVENT_WIFI_AP_START:
+            Serial.println("--- WiFi AP iniciado");
+            break;
+        case ARDUINO_EVENT_WIFI_AP_STOP:
+            Serial.println("--- WiFi AP parou");
+            break;
+        case ARDUINO_EVENT_WIFI_AP_STACONNECTED:
+            Serial.println("--- Cliente conectou");
+            break;
+        case ARDUINO_EVENT_WIFI_AP_STADISCONNECTED:
+            Serial.println("--- Cliente desconectou");
+            break;
+        default: break;
+    }
 }
